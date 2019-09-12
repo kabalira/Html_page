86d3e8b81e64211b342d7594ca7483c709833e1d
kabalira <55194798+kabalira@users.noreply.github.com>
2019-09-12 18:23:15 +0530

    Add files via upload

--- Employee/display values on different page/test.js
+++ Employee/display values on different page/test.js
@@ -1,34 +1,86 @@
 
-function newform() {	
-	if (document.forms["form"]["ename"].value == "") {
-		alert("Name must be filled out");
-		return false;
-	}else if (document.forms["form"]["edepartment"].value == "") {
-		alert("Department must be filled out");
-		return false;
-	}else if (document.forms["form"]["esalary"].value == "") {
-		alert("Salary must be filled out");
-		return false;
-	}else if (document.forms["form"]["ephone"].value == "") {
-		alert("Phone Number must be filled out");
-		return false;
-	}else if (document.forms["form"]["eemail"].value == "") {
-		alert("Email must be filled out");
-		return false;
-	}
-	else{					
-		var name = document.getElementById("ename").value;
-		sessionStorage.setItem("EmpName",name);
-		var department = document.getElementById("edepartment").value;
-		sessionStorage.setItem("EmpDepartment",department);
-		var salary = document.getElementById("esalary").value;
-		sessionStorage.setItem("EmpSalary",salary);
-		var phone = document.getElementById("ephone").value;
-		sessionStorage.setItem("EmpPhoneNumber",phone);
-		var email = document.getElementById("eemail").value;
-		sessionStorage.setItem("EmpEmail",email);
-		var datetime = document.getElementById("edatetime").value;
-		sessionStorage.setItem("EmpDateTime",datetime);
-		window.open('test2.html');
-	}
-}
+$(document).ready(function(){
+	$("input#edepartment").click(function(){
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}
+	});
+	$("input#esalary").click(function(){
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}else if($("#edepartment").val() == ""){
+			alert("Department must be filled out");
+		}
+	});
+	$("input#ephone").click(function(){
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}else if($("#edepartment").val() == ""){
+			alert("Department must be filled out");
+		}else if($("#esalary").val() == ""){
+			alert("Salary must be filled out");
+		}
+	});
+	$("input#eemail").click(function(){
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}else if($("#edepartment").val() == ""){
+			alert("Department must be filled out");
+		}else if($("#esalary").val() == ""){
+			alert("Salary must be filled out");
+		}
+	});
+	$("input#datepicker").click(function(){
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}else if($("#edepartment").val() == ""){
+			alert("Department must be filled out");
+		}else if($("#esalary").val() == ""){
+			alert("Salary must be filled out");
+		}else {
+			$( "#datepicker" ).datepicker();
+		}
+	});
+});
+
+// $(function() {
+// 	$( "#datepicker" ).datepicker();
+// });
+
+$(document).ready(function(){
+	$("#submit_button").click(function() { 
+		if($("#ename").val() == ""){
+			alert("Name must be filled out");
+		}
+		else if($("#edepartment").val() == ""){
+			alert("Name must be filled out");
+		}
+		else if($("#esalary").val() == ""){
+			alert("Name must be filled out");
+		}
+		else if($("#ephone").val() == ""){
+			alert("Name must be filled out");
+		}
+		else if($("#eemail").val() == ""){
+			alert("Name must be filled out");
+		}
+		else if($("#datepicker").val() == ""){
+			alert("Name must be filled out");
+		}
+		else {
+			var name = $("#ename").val();
+			sessionStorage.setItem("EmpName",name);
+			var department = $("#edepartment").val();
+			sessionStorage.setItem("EmpDepartment",department);
+			var salary = $("#esalary").val();
+			sessionStorage.setItem("EmpSalary",salary);
+			var phone = $("#ephone").val();
+			sessionStorage.setItem("EmpPhoneNumber",phone);
+			var email = $("#eemail").val();
+			sessionStorage.setItem("EmpEmail",email);
+			var datetime = $("#datepicker").val();
+			sessionStorage.setItem("EmpDateTime",datetime);
+			window.open('test2.html');
+		}
+	});
+});
